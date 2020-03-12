dda2969d593b1e4288b00abe7a215d1ddbd80e31
Jose Mascaro <jose.mascaro@oktana.io>
2020-03-12 16:32:10 -0500

    lesson 16

--- app/src/main/java/com/example/josemascaro/smack/Controller/LoginActivity.kt
+++ app/src/main/java/com/example/josemascaro/smack/Controller/LoginActivity.kt
@@ -1,16 +1,23 @@
 package com.example.josemascaro.smack.Controller
 
+import android.content.Context
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
 import com.example.josemascaro.smack.R
+import com.example.josemascaro.smack.Services.AuthService
+import kotlinx.android.synthetic.main.activity_create_user.*
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        loginSpinner.visibility = View.INVISIBLE
     }
 
     fun loginCreateUserBtnClicked(view: View){
@@ -21,6 +28,60 @@ class LoginActivity : AppCompatActivity() {
     }
 
     fun loginLoginBtnClicked(view: View){
+
+        enableSpinner(true)
+        val email = loginEmailTxt.text.toString()
+        val password = loginPasswordTxt.text.toString()
+
+        hideKeyboard()
+
+        if(email.isNotEmpty() && password.isNotEmpty()){
+            AuthService.loginUser(this,email,password){ loginSuccess ->
+                if(loginSuccess){
+                    AuthService.findUserByEmail(this){ findSuccess ->
+                        if(findSuccess){
+                            enableSpinner(false)
+                            finish()
+                        }else{
+                            errorToast()
+                        }
+
+                    }
+                }else{
+                    errorToast()
+                }
+            }
+        }else{
+            Toast.makeText(this,"Please make sure you filled all in",Toast.LENGTH_SHORT).show()
+        }
+
+
+    }
+
+    fun errorToast(){
+        Toast.makeText(this, "Something when wrong please try again", Toast.LENGTH_SHORT).show()
+        enableSpinner(false)
+    }
+
+    fun enableSpinner(enable : Boolean){
+
+        if (enable){
+            loginSpinner.visibility = View.VISIBLE
+        }else{
+            loginSpinner.visibility = View.INVISIBLE
+        }
+
+        loginLoginBtn.isEnabled = !enable
+        loginCreateUserBtn.isEnabled = !enable
+
+    }
+
+    fun hideKeyboard(){
+        val inputManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+
+        if(inputManager.isAcceptingText){
+            inputManager.hideSoftInputFromWindow(currentFocus.windowToken,0)
+        }
 
     }
 }
