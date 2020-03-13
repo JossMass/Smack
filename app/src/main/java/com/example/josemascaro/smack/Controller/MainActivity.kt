57785855f8ea5493287c58b497429a8074c4e4b0
Jose Mascaro <jose.mascaro@oktana.io>
2020-03-13 17:22:03 -0500

    lesson 18

--- app/src/main/java/com/example/josemascaro/smack/Controller/MainActivity.kt
+++ app/src/main/java/com/example/josemascaro/smack/Controller/MainActivity.kt
@@ -13,6 +13,7 @@ import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
 import android.view.View
 import android.view.inputmethod.InputMethodManager
+import android.widget.ArrayAdapter
 import android.widget.EditText
 import com.example.josemascaro.smack.Model.Channel
 import com.example.josemascaro.smack.R
@@ -30,6 +31,12 @@ import kotlinx.android.synthetic.main.nav_header_main.*
 class MainActivity : AppCompatActivity(){
 
     val socket = IO.socket(SOCKET_URL)
+    lateinit var channelAdapter : ArrayAdapter<Channel>
+
+    private fun setupAdapters(){
+        channelAdapter = ArrayAdapter(this,android.R.layout.simple_list_item_1,MessageService.channels)
+        channel_list.adapter = channelAdapter
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -43,6 +50,8 @@ class MainActivity : AppCompatActivity(){
                 this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
         drawer_layout.addDrawerListener(toggle)
         toggle.syncState()
+
+        setupAdapters()
 
 
     }
@@ -66,7 +75,7 @@ class MainActivity : AppCompatActivity(){
 
 
     private val userDataChangeReceiver = object : BroadcastReceiver (){
-        override fun onReceive(context : Context?, intent : Intent?) {
+        override fun onReceive(context : Context, intent : Intent?) {
             if(AuthService.isLoggedIn){
                 userNameNavHeader.text = UserDataService.name
                 userEmailNavHeader.text = UserDataService.email
@@ -75,6 +84,12 @@ class MainActivity : AppCompatActivity(){
                 userImageNavHeader.setImageResource(resourceId)
                 userImageNavHeader.setBackgroundColor(UserDataService.returnAvatarColor(UserDataService.avatarColor))
                 loginBtnNavHeader.text = "Logout"
+
+                MessageService.getChannels(context){ complete ->
+                    if(complete){
+                        channelAdapter.notifyDataSetChanged()
+                    }
+                }
             }
         }
     }
--- app/src/main/java/com/example/josemascaro/smack/Services/MessageService.kt
+++ app/src/main/java/com/example/josemascaro/smack/Services/MessageService.kt
@@ -1,6 +1,13 @@
 package com.example.josemascaro.smack.Services
 
+import android.content.Context
+import android.util.Log
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonArrayRequest
+import com.android.volley.toolbox.Volley
 import com.example.josemascaro.smack.Model.Channel
+import com.example.josemascaro.smack.Utilities.URL_GET_CHANNELS
+import org.json.JSONException
 
 /**
  * Created by josemascaro on 3/13/20.
@@ -8,5 +15,48 @@ import com.example.josemascaro.smack.Model.Channel
 object MessageService {
 
     var channels = ArrayList<Channel>()
+
+    fun getChannels(context: Context, complete : (Boolean) -> Unit){
+
+        val channelRequest = object : JsonArrayRequest(Method.GET, URL_GET_CHANNELS,null,Response.Listener { response ->
+
+            try {
+
+                for(x in 0 until response.length()){
+
+                    val channel = response.getJSONObject(x)
+                    val name = channel.getString("name")
+                    val chanDesc = channel.getString("description")
+                    val channelId = channel.getString("_id")
+
+                    val newChannel = Channel(name, chanDesc, channelId)
+                    this.channels.add(newChannel)
+                }
+
+            }catch (e : JSONException){
+                Log.d("JSON","EXC: " + e.localizedMessage)
+
+            }
+
+
+        }, Response.ErrorListener { error ->
+            Log.d("ERROR ", "Could not retrieve channels")
+            complete(false)
+
+        }){
+
+            override fun getBodyContentType(): String {
+                return "application/json; charset=utf-8"
+            }
+
+            override fun getHeaders(): MutableMap<String, String> {
+                val headers = HashMap<String,String>()
+                headers.put("Authorization","Bearer ${AuthService.authToken}")
+                return headers
+            }
+        }
+
+        Volley.newRequestQueue(context).add(channelRequest)
+    }
 
 }
