9566aa95fa58a4154babb27eff5edf4128d9e4e9
Jose Mascaro <jose.mascaro@oktana.io>
2020-03-16 13:32:50 -0500

    Initial commit2

---
+++ app/src/main/java/com/example/jonnyb/smack/Utilities/Constants.kt
@@ -0,0 +1,17 @@
+package com.example.jonnyb.smack.Utilities
+
+/**
+ * Created by jonnyb on 9/1/17.
+ */
+
+//const val BASE_URL = "https://devslopes-chattin.herokuapp.com/v1/"
+const val BASE_URL = "http://10.0.2.2:3005/v1/"
+const val SOCKET_URL = "http://10.0.2.2:3005/"
+const val URL_REGISTER = "${BASE_URL}account/register"
+const val URL_LOGIN = "${BASE_URL}account/login"
+const val URL_CREATE_USER = "${BASE_URL}user/add"
+const val URL_GET_USER = "${BASE_URL}user/byEmail/"
+const val URL_GET_CHANNELS = "${BASE_URL}channel/"
+
+// Broadcast Constants
+const val BROADCAST_USER_DATA_CHANGE = "BROADCAST_USER_DATA_CHANGE"
